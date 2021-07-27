6a0cbfd0b82aa85952b7e3bb4a03cfb06b20cade
abana <neroabana@gmail.com>
2021-07-27 14:47:13 +0100

    removed line before last line

--- time.js
+++ time.js
@@ -1,9 +1,6 @@
-
 const hotpOtpGenerator = require('hotp-totp-generator');
 
 const totpToken = hotpOtpGenerator.totp({ key: 'neroabana@gmail.comHENNGECHALLENGE003',
 T0: "0", X: "30", algorithm: "sha512", digits: "10" 
-
 });
-
 console.log(totpToken);
