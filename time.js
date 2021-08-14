70df4d25aeaf53b5c9f76e21dc4e367b6ddd4255
abana <neroabana@gmail.com>
2021-08-14 08:37:03 +0100

    edited time

--- time.js
+++ time.js
@@ -1,5 +1,4 @@
 
-
 const hotpOtpGenerator = require('hotp-totp-generator');
 const totpToken = hotpOtpGenerator.totp({ key: 'neroabana@gmail.comHENNGECHALLENGE003',
 T0: "0", X: "30", algorithm: "sha512", digits: "10" 
